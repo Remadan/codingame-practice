78355b1df02ae686d4afba549d581ad76f7c6dcf
Remadan <remadan@gmail.com>
2017-04-21 15:05:41 +0300

    20170421

--- training.medium/the-last-crusade-episode-1.cs
+++ training.medium/the-last-crusade-episode-1.cs
@@ -13,6 +13,13 @@ class Player
 {
     static void Main(string[] args)
     {
+
+        Dictionary<int, RoomLayout> roomTypes = DefaultRoomTypes();
+
+
+
+
+
         string[] inputs;
         inputs = Console.ReadLine().Split(' ');
         int dungeonWidth = int.Parse(inputs[0]); // number of columns.
@@ -38,24 +45,87 @@ class Player
             Console.WriteLine("0 0");
         }
     }
+
+    static Dictionary<int, RoomLayout> DefaultRoomTypes()
+    {
+        // UP       - 0
+        // RIGHT    - 1
+        // BOTTOM   - 2
+        // LEFT     - 3
+
+        Dictionary<int, RoomLayout> roomTypes = new Dictionary<int, RoomLayout>();
+
+        for (int i = 0; i < 14; i++)
+            roomTypes.Add(i, new RoomLayout());
+
+        roomTypes[1].AddEntryPoint(0, 2);
+        roomTypes[1].AddEntryPoint(1, 2);
+        roomTypes[1].AddEntryPoint(3, 2);
+
+        roomTypes[2].AddEntryPoint(1, 3);
+        roomTypes[2].AddEntryPoint(3, 1);
+
+        roomTypes[3].AddEntryPoint(0, 2);
+
+        roomTypes[4].AddEntryPoint(0, 3);
+        roomTypes[4].AddEntryPoint(1, 2);
+
+        roomTypes[5].AddEntryPoint(0, 1);
+        roomTypes[5].AddEntryPoint(3, 2);
+
+        roomTypes[6].AddEntryPoint(1, 3);
+        roomTypes[6].AddEntryPoint(3, 1);
+
+        roomTypes[7].AddEntryPoint(0, 2);
+        roomTypes[7].AddEntryPoint(1, 2);
+
+        roomTypes[8].AddEntryPoint(1, 2);
+        roomTypes[8].AddEntryPoint(3, 2);
+
+        roomTypes[9].AddEntryPoint(0, 2);
+        roomTypes[9].AddEntryPoint(3, 2);
+
+        roomTypes[10].AddEntryPoint(0, 3);
+
+        roomTypes[11].AddEntryPoint(0, 1);
+
+        roomTypes[12].AddEntryPoint(1, 2);
+
+        roomTypes[13].AddEntryPoint(3, 2);
+
+        return roomTypes;
+    }
 }
 
-class roomLayout
+class RoomLayout
 {
     private Dictionary<int, List<int>> entryPoints;
 
-    public roomLayout()
+    public RoomLayout()
     {
         entryPoints = new Dictionary<int, List<int>>();
     }
 
-    public List<int> AvaylibleExits(int entryPoint)
+    public List<int> AvailableExits(int entryPoint)
     {
-        List<>
-        if (entryPoints.TryGetValue(entryPoint, out ))
+        List<int> result;
+        if (!entryPoints.TryGetValue(entryPoint, out result))
         {
-
+            result = null;
         }
+        return result;
+    }
+
+    public void AddEntryPoint(int entryPoint, List<int> exits)
+    {
+        entryPoints.Add(entryPoint, exits);
+    }
+
+    public void AddEntryPoint(int entryPoint, int onlyExit)
+    {
+        List<int> exits = new List<int>();
+        exits.Add(onlyExit);
+        entryPoints.Add(entryPoint, exits);
     }
 
 }
